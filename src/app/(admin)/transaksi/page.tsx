69d39440c17f3a643ea205fa9fb795f39ec06183
hariando7 <rianaja2200022@gmail.com>
2024-12-23 01:59:15 +0700

    fix: transaction history

--- src/app/(admin)/transaksi/page.tsx
+++ src/app/(admin)/transaksi/page.tsx
@@ -2,7 +2,7 @@
 
 import React, { useState } from "react";
 import { Label } from "@/components/ui/label";
-import { Input } from "@/components/ui/input";
+// import { Input } from "@/components/ui/input";
 import {
   Table,
   TableBody,
