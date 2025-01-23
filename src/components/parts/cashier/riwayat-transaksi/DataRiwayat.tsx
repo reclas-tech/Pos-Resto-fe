8eae4b75baaf34fbe28c3a8346d3de3efb55c3c4
PanduWahyudi <panduwahyudi12@gmail.com>
2025-01-24 00:29:14 +0700

    fix: fix bug slicing riwayat transaksi

--- src/components/parts/cashier/riwayat-transaksi/DataRiwayat.tsx
+++ src/components/parts/cashier/riwayat-transaksi/DataRiwayat.tsx
@@ -198,7 +198,8 @@ const DataRiwayat: React.FC<ListApiResponse> = ({ data }) => {
   }
   return (
     <>
-      <section className="grid grid-cols-3 gap-4">
+      {/* Before Revisi Pandu */}
+      {/* <section className="grid grid-cols-3 gap-4">
         {data.map((item, index) => {
           return (
             <>
@@ -250,6 +251,63 @@ const DataRiwayat: React.FC<ListApiResponse> = ({ data }) => {
             </>
           );
         })}
+      </section> */}
+      {/* After Pandu Revisi */}
+      <section className="grid grid-cols-3 gap-4">
+        {data.map((item, index) => (
+          <div
+            key={item?.id || index}
+            className="flex border-primaryColor border rounded-lg p-2"
+          >
+            <div className="flex items-center space-x-4 w-full">
+              <button
+                className="flex-shrink-0 rounded-lg border border-secondaryColor flex items-center justify-center 
+          aspect-square w-28 h-28 font-bold text-sm text-black text-center break-words px-2"
+              >
+                {item?.customer}
+              </button>
+              <div className="flex-grow min-w-0 space-y-2">
+                <p className="truncate font-semibold">{item?.code}</p>
+                <div>26/02/2023 09:46:00</div>
+                <p className="font-medium">
+                  {formatRupiah(item?.price_sum.toLocaleString())}
+                </p>
+                <div className="flex gap-2">
+                  <Button
+                    variant="ghostButton"
+                    className={`text-sm text-white pl-2 pr-2 pt-1 pb-1 h-fit w-full ${
+                      item?.status === "success"
+                        ? "bg-secondaryColor"
+                        : item?.status === "pending"
+                        ? "bg-primaryColor"
+                        : "bg-[#FF0000]"
+                    }`}
+                  >
+                    {item?.status === "success"
+                      ? "Berhasil"
+                      : item?.status === "pending"
+                      ? "Tertunda"
+                      : "Gagal"}
+                  </Button>
+                  <Button
+                    variant="ghostButton"
+                    onClick={() => {
+                      setSelectedTransactionId(item?.id);
+                      if (item?.status === "pending") {
+                        setIsEditModalOpen(true);
+                      } else {
+                        setIsDetailModalOpenDineIn(true);
+                      }
+                    }}
+                    className="text-sm bg-white border border-secondaryColor text-black pl-2 pr-2 pt-1 pb-1 h-fit w-full"
+                  >
+                    Detail
+                  </Button>
+                </div>
+              </div>
+            </div>
+          </div>
+        ))}
       </section>
       <DetailModal
         isOpen={isDetailModalOpenDineIn}
