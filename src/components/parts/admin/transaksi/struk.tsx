d452911d6412a74de19e4fea63a1c24eac033006
RobiHardinata19 <robi.120140034@student.itera.ac.id>
2025-01-26 11:00:16 +0700

    feat: print detail transaksiinvoice admin

---
+++ src/components/parts/admin/transaksi/struk.tsx
@@ -0,0 +1,72 @@
+/* eslint-disable react/display-name */
+import React from "react";
+import { useGetTransaksiOne } from "./api";
+
+type PropsInvoice = {
+    idInvoice: string;
+};
+
+const TableReceiptAdmin = React.forwardRef<HTMLDivElement, PropsInvoice>((props, ref) => {
+    const {
+        idInvoice
+    } = props;
+    
+    // Data fetching
+    const { data: dataDetail } = useGetTransaksiOne(idInvoice);
+
+    if (!dataDetail?.data) return null;
+
+    return (
+        <div ref={ref} id="struk" className="bg-white p-[2%] text-[10px] w-[250px]">
+            <p className="uppercase text-center font-medium">Checker</p>
+
+            <div className="text-left mt-6 grid grid-cols-2 pb-2 border-b border-dashed border-black">
+                <p>No Transaksi</p>
+                <p>: {dataDetail?.data?.code}</p>
+                <p>Waktu</p>
+                <p>: {new Date(dataDetail?.data?.created_at).toLocaleString()}</p>
+                <p>Nama Pemesan</p>
+                <p>: {dataDetail?.data?.cashier}</p>
+                <p>Nomor Meja</p>
+                <p>: {dataDetail?.data?.tables.join(", ")}</p>
+            </div>
+
+            <div className="mt-2 space-y-2 border-b border-dashed border-black pb-2">
+                {dataDetail?.data?.products.map((product, index) => (
+                    <div key={index}>
+                        <span className="flex space-x-[2%]">
+                            <p>{product.quantity}x</p>
+                            <p>{product.name}</p>
+                        </span>
+                        {/* {product.note && product.note.trim() !== "" && (
+              <ul className="list-disc ml-6">
+                <li className="">{product.note}</li>
+              </ul>
+            )} */}
+                    </div>
+                ))}
+
+                {dataDetail?.data?.packets.map((packet, index) => (
+                    <div key={index}>
+                        <span className="flex space-x-[2%]">
+                            <p>{packet.quantity}x</p>
+                            <p>{packet.name}</p>
+                        </span>
+                        {/* {packet.note && packet.note.trim() !== "" && (
+              <ul className="list-disc ml-6">
+                <li className="">{packet.note}</li>
+              </ul>
+            )} */}
+                    </div>
+                ))}
+            </div>
+
+            <p className="text-center mt-4">
+                {new Date(dataDetail?.data?.created_at).toLocaleString()}
+            </p>
+            <p className="text-center">Cetak : Waroeng Aceh Garuda</p>
+        </div>
+    );
+});
+
+export default TableReceiptAdmin;
